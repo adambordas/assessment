9c1be5e28d63280f05af0bd0b9037d9e992e912a
Ádám Bordás <adam.bordas@drukka.hu>
2020-07-14 11:46:37 +0200

    Set up dom

--- js-numerals/src/App.js
+++ js-numerals/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Alert, Container, InputGroup, FormControl, Button } from 'react-bootstrap';
 
-function App() {
+export function convertNumerals(number) {
+  return "zero";
+};
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const [englishNumeral, setEnglishNumeral] = useState('zero');
+
+  const numberChangeHandler = e => {
+    setNumber(e.target.value);
+  };
+
+  const submitHandler = () => {
+    setEnglishNumeral(convertNumerals(number));
+  };
+
   return (
-    <>
-      
-    </>
+    <Container className='pt-5'>
+      <h1 className='mb-3'>JS Numerals</h1>
+      <Alert variant='dark'>
+        {englishNumeral}
+      </Alert>
+      <InputGroup>
+        <FormControl
+          type='number'
+          value={number}
+          onChange={numberChangeHandler}
+          min={0}
+          max={Number.MAX_SAFE_INTEGER}
+        />
+        <InputGroup.Append>
+          <Button variant='primary' onClick={submitHandler}>Submit</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </Container>
   );
 }
-
-export default App;
