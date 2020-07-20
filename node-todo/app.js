cebc4529879186bb44a35b95580fce2aa74f9fc4
Ádám Bordás <adam.bordas@drukka.hu>
2020-07-20 14:19:45 +0200

    Create controllers and routes

--- node-todo/app.js
+++ node-todo/app.js
@@ -3,9 +3,16 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 
 const app = express();
+const todoController = require('./controllers/todos.controller.js');
 
 app.use(helmet());
 app.use(bodyParser.json());
+
+app.post('/todos', todoController.create);
+app.get('/todos', todoController.list);
+app.get('/todos/:id', todoController.get);
+app.put('/todos/:id', todoController.update);
+app.delete('/todos/:id', todoController.delete);
 
 const HTTP_PORT = process.env.HTTP_PORT || 8000;
 app.listen(HTTP_PORT, () => {
---
+++ node-todo/controllers/todos.controller.js
@@ -0,0 +1,80 @@
+const todoService = require('../services/todos.service.js');
+
+exports.create = (req, res) => {
+  const { text, priority } = req.body;
+  if (!text) {
+    return res.status(400).json({ error: 'Text parameter is required.' });
+  }
+
+  if (priority && Number.isInteger(priority) && [1, 2, 3, 4, 5].indexOf(priority) < 0) {
+    return res.status(400).json({ error: 'Priority has to be an integer between 1 and 5.' });
+  }
+
+  const todo = todoService.create(text, priority);
+
+  res.status(201).json(todo);
+};
+
+exports.list = (req, res) => {
+  const todos = todoService.list();
+
+  if (todos.length === 0) {
+    return res.sendStatus(204);
+  }
+
+  res.json(todos);
+};
+
+exports.get = (req, res) => {
+  const todo = todoService.get(req.params.id);
+
+  if (!todo) {
+    return res.status(404).json({ error: 'Task not found.' });
+  }
+
+  res.json(todo);
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  const { text, priority, done } = req.body;
+  if (!text) {
+    return res.status(400).json({ error: 'Text parameter is required.' });
+  }
+
+  if (typeof done !== 'boolean') {
+    return res.status(400).json({ error: 'Done parameter is required' });
+  }
+
+  if (priority && Number.isInteger(priority) && [1, 2, 3, 4, 5].indexOf(priority) < 0) {
+    return res.status(400).json({ error: 'Priority has to be an integer between 1 and 5.' });
+  }
+
+  try {
+    const todo = todoService.update(id, text, priority, done);
+
+    res.json(todo);
+  } catch (error) {
+    if (error.message === 'Task not found.') {
+      return res.status(404).json({ error: error.message });
+    }
+
+    throw error;
+  }
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  try {
+    todoService.remove(id);
+
+    res.json({});
+  } catch (error) {
+    if (error.message === 'Task not found.') {
+      return res.status(404).json({ error: error.message });
+    }
+
+    throw error;
+  }
+};
